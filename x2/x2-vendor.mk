d3dfc1a473d6444946ae57e3d12193ad70bc6cb9
1nfrag <sil3ntcll@gmail.com>
2017-10-26 17:40:22 +0800

    re-addd libminui 32bit
    Change-Id: I337282cdc08ac2349a89e5447da6cd8b756c4911

--- x2/x2-vendor.mk
+++ x2/x2-vendor.mk
@@ -163,6 +163,7 @@ PRODUCT_COPY_FILES += \
     vendor/leeco/x2/proprietary/lib/libqomx_core.so:system/lib/libqomx_core.so \
     vendor/leeco/x2/proprietary/lib/libqrDec.so:system/lib/libqrDec.so \
     vendor/leeco/x2/proprietary/lib64/hw/fingerprint.msm8996.so:system/lib64/hw/fingerprint.msm8996.so \
+    vendor/leeco/x2/proprietary/lib/libminui.so:system/lib/libminui.so \
     vendor/leeco/x2/proprietary/lib64/libminui.so:system/lib64/libminui.so \
     vendor/leeco/x2/proprietary/vendor/etc/audio_output_policy_letv.conf:system/vendor/etc/audio_output_policy_letv.conf \
     vendor/leeco/x2/proprietary/vendor/lib/lib-dplmedia.so:system/vendor/lib/lib-dplmedia.so \
