a4b25a2f10349dbbdd0347d7be0b585a3ae9b11c
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2019-01-20 02:23:20 +0000

    msm8996-common: implement binderized consumerir HAL + bridge
    Since LeEco doesn't use any driver blobs for consumerir and instead
    uses a quickset SDK, we need a service that acts as a bridge to the
    quickset SDK for translating calls from our consumerir  HAL,  which
    we named "ConsumerirTransmitter".
    
    How this works is our consumerir HAL receives an IR command,  which
    it  sends  to the ConsumerirTransmitter that translates it  to  the
    quickset SDK.
    
    Change-Id: Ib21a70708cdfb2c7f6a53c8a1c6ddcac830a521e

--- msm8996-common/Android.mk
+++ msm8996-common/Android.mk
@@ -228,7 +228,7 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := WfdCommon
+LOCAL_MODULE :=:= WfdCommon
 LOCAL_MODULE_OWNER := leeco
 LOCAL_SRC_FILES := proprietary/framework/WfdCommon.jar
 LOCAL_CERTIFICATE := platform
@@ -266,6 +266,18 @@ LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := UEIQuicksetSDKLeTV
+LOCAL_MODULE_OWNER := leeco
+LOCAL_SRC_FILES := proprietary/vendor/app/UEIQuicksetSDKLeTV/UEIQuicksetSDKLeTV.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 endif
